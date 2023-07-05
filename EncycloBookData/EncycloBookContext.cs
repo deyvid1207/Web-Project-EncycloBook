cf08ee8403540b32bfb41cc24d97d397f6b96d17
drn12 <drn12@DESKTOP-E8Q899P>
2023-07-05 08:00:32 +0300

    Add project files.

---
+++ EncycloBookData/EncycloBookContext.cs
@@ -0,0 +1,28 @@
+﻿using EncycloBookData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncycloBookData
+{
+    public class EncycloBookContext : DbContext
+    {
+        DbSet<Animal> Animals { get; set; } = null!;
+        DbSet<Bacteria> Bacteria { get; set; } = null!;
+        DbSet<Comment> Comments { get; set; } = null!;
+        DbSet<Fungus> Fungi { get; set; } = null!;
+        DbSet<Plant> Plants { get; set; } = null!;
+        DbSet<Virus> Viruses { get; set; } = null!;
+
+        public EncycloBookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected EncycloBookContext()
+        {
+        }
+    }
+}
---
+++ EncycloBookData/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncycloBookData.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string Content { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "yyyy-MM-dd", ApplyFormatInEditMode = true)]
+        public DateTime PublishedOn { get; set; }
+    }
+}

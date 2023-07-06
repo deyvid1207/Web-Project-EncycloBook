5988432a347ff2b1debfb8fe91d9611ae56e3a07
deyvid1207 <81879561+deyvid1207@users.noreply.github.com>
2023-07-06 13:34:32 +0300

    Added user special home-page, about page and fixed the nav-bar

--- EncycloBook/Controllers/HomeController.cs
+++ EncycloBook/Controllers/HomeController.cs
@@ -26,7 +26,10 @@ namespace EncycloBook.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ EncycloBook/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EncycloBook.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}

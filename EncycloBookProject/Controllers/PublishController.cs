70380da9d46d43b535b885beb3eb887a9aed0b81
deyvid1207 <81879561+deyvid1207@users.noreply.github.com>
2023-07-16 16:14:20 +0300

    Continued work on publish page, added publish for Animal, started work on Services

---
+++ EncycloBookProject/Controllers/PublishController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EncycloBookProject.Controllers
+{
+    public class PublishController : Controller
+    {
+        [HttpPost]
+        public IActionResult RedirectAct()
+        {
+            string selectedValue = Request.Form["pref"];
+            switch (selectedValue)
+            {
+                case "Animal":
+                    return RedirectToAction("Animal");
+                default:
+                    return RedirectToPage("/");
+     
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Animal()
+        {
+            return View("Animal");
+        }
+    }
+}
